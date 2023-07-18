1164f58d2190372eca062bdd64c3d17dc27d6273
eduardovisa <eduardo.mvisa@gmail.com>
2023-07-18 17:13:52 -0600

    Adding contentful api

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,19 @@
 import heroImg from '../assets/heroImg.svg';
+import reactLogo from '../assets/reactLogo.svg';
 
 const Hero = () => {
   return (
     <section className="hero">
       <div className="hero-center">
         <div className="hero-title">
-          <h1>React Proyectos</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
-            corrupti aperiam, laborum totam perferendis odio nostrum veritatis
-            sint commodi quas placeat dolor quae aut quam deserunt, eos vero
-            aspernatur? Maxime.
-          </p>
+          <h2>
+            React - Proyectos <img src={reactLogo} alt="reactLogo" />
+          </h2>
+          <h5>
+            Portafolio de proyectos realizados con la tecnología React desde
+            básicos hasta más complejo en los cuales se puede revisar su código
+            y un live demo del mismo.
+          </h5>
         </div>
         <div className="img-container">
           <img src={heroImg} alt="heroImg" className="img" />
---
+++ src/data.ts
@@ -0,0 +1,15 @@
+interface Projects {
+  title: string;
+  url: string;
+  image: string;
+}
+
+const projects: Projects[] = [
+  {
+    title: 'sd',
+    image: ' sdsa',
+    url: 'sda',
+  },
+];
+
+export default projects;
