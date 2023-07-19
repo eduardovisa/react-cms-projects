18df416ed8986943574c9fdacb035c1dacd7cc88
eduardovisa <eduardo.mvisa@gmail.com>
2023-07-18 18:28:11 -0600

    Fetching data and adding cards

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,44 @@
+import { useFetchProjects } from '../fetchProjects';
+
 const Projects = () => {
-  return <section>Projects</section>;
+  const { loading, projects } = useFetchProjects();
+  // console.log(loading, projects);
+
+  if (loading) {
+    return (
+      <section className="projects">
+        <h2>Cargando...</h2>
+      </section>
+    );
+  }
+
+  return (
+    <section className="projects">
+      <div className="title">
+        <h2>proyectos</h2>
+        <div className="title-underline"></div>
+      </div>
+      <div className="projects-center">
+        <ul class="cards">
+          {projects.map((project) => {
+            const { id, title, urlCode, urlDemo, img } = project;
+
+            return (
+              <a
+                key={`card-${id}`}
+                href={urlCode}
+                target="_blank"
+                rel="noreferrer"
+                className="project"
+              >
+                <img src={img.fields.file.url} alt="" />
+                <h5>{title}</h5>
+              </a>
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+  );
 };
 export default Projects;
--- src/fetchProjects.tsx
+++ src/fetchProjects.tsx
@@ -1,4 +1,5 @@
 import { createClient } from 'contentful';
+import { useState, useEffect } from 'react';
 
 const client = createClient({
   space: 'xu9i935s8wkm',
@@ -6,7 +7,43 @@ const client = createClient({
   accessToken: 'ojI7KQZ2VYgT_LAOLxeDxl-zfPmcrQ35LSKCmCeqzHM',
 });
 
-client
-  .getEntries({ content_type: 'projects' })
-  .then((response) => console.log(response))
-  .catch((error) => console.log(error));
+interface Projects {
+  id: number;
+  title: string;
+  urlCode: string;
+  urlDemo: string;
+  img: unknown;
+}
+
+export const useFetchProjects = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [projects, setProjects] = useState<Projects[]>([]);
+
+  const getData = async () => {
+    try {
+      const response = await client.getEntries({
+        content_type: 'projects',
+      });
+
+      const projects = response.items.map((item) => {
+        const { title, urlCode, urlDemo, image } = item.fields;
+        const id = parseInt(item.sys.id);
+        const img = image;
+
+        return { id, title, urlCode, urlDemo, img };
+      });
+
+      setProjects(projects);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    void getData();
+  }, []);
+
+  return { loading, projects };
+};
